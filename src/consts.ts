0ed6539a783253482d4865f9b985cf31ef5f7dcf
Алина Вдовиченко <alinavdovichenko.ru@gmail.com>
2024-09-27 09:12:35 +0500

    1. Инициализирует проект.

---
+++ src/consts.ts
@@ -0,0 +1,10 @@
+
+export enum AppRoute {
+  Main = '/',
+  Register = '/register',
+  Add = '/add',
+  Edit = '/edit/:id',
+  Catalog = '/products',
+  Product = '/products/:id',
+  NotFound ='*',
+}
---
+++ src/pages/main-page/main-page.tsx
@@ -0,0 +1,10 @@
+import {Helmet} from 'react-helmet-async';
+import Footer from '../components/footer/footer';
+import Header from '../components/header/header';
+import { Link } from 'react-router-dom';
+
+function MainPage(): JSX.Element {
+  return (
+  );
+}
+export default MainPage;
